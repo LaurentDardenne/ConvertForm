33b7b521be2e48022ae92fd3311f0ba3156d8892
LaurentDardenne <LaDardenne@Orange.fr>
2014-09-07 18:09:59 +0200

    Jeux de test : validation des noms de chemin

---
+++ Tests/Convert-Form/PathParameters.Tests.ps1
@@ -0,0 +1,73 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+$DesignerFileExisting ='$($ConvertForm.RepositoryLocation)\TestsWinform\Base\Form1.Designer.cs'
+
+Describe "PathParameters" {
+   Context "Le paramètre Source doit être un nom de fichier" {
+     It "fails" {
+        Convert-Form -Source .. -Destination .. -ErrorAction SilentlyContinue| Should Throw
+     }
+   }
+
+   Context "Le paramètre Source doit être un nom de fichier" {
+     It "fails" {
+        Convert-Form -Source . -Destination ..| Should Throw
+     }
+   }
+
+   Context "Le paramètre Destination doit être un nom de fichier" {
+     It "fails" {
+        Convert-Form -Source $DesignerFileExisting -Destination .| Should Throw
+     }
+   }
+
+   Context "Le paramètre Destination doit être un nom de fichier" {
+     It "fails" {
+        Convert-Form -Source $DesignerFileExisting -Destination ..| Should Throw
+     }
+   }
+
+   Context "Le fichier n'existe pas" {
+     It "fails" {
+        Convert-Form -Source 'c:\temp\NotExist.Designer.cs' -Destination .| Should Throw
+     }
+   }
+
+   Context "Le fichier n'existe pas" {
+     It "fails" {
+        Convert-Form -Source 'c:\NotExist\MainForm.Designer.cs' -Destination .| Should Throw
+     }
+   }
+
+   Context "Le lecteur indiqué n'existe pas" {
+     It "fails" {
+        Convert-Form -Source 'H:\PS\MainForm.Designer.cs' -Destination .| Should Throw
+     }
+   }
+
+   Context "Le chemin doit pointer sur le FileSystem" {
+     It "fails" {
+        Convert-Form -Source 'Hklm:\Software\MainForm.Designer.cs' -Destination .| Should Throw
+     }
+   }
+
+   Context "Le répertoire n'existe pas" {
+     It "fails" {
+        Convert-Form -Source $DesignerFileExisting -Destination 'C:\Notexist\Frm1.ps1'| Should Throw
+     }
+   }
+
+   Context "Le lecteur indiqué n'existe pas" {
+     It "fails" {
+        Convert-Form -Source $DesignerFileExisting -Destination 'H:\Temp\Exist.ps1'| Should Throw
+     }
+   }
+
+   Context "Le chemin doit pointer sur le FileSystem" {
+     It "fails" {
+        Convert-Form -Source $DesignerFileExisting -Destination 'Hklm:\Software\frm.ps1'| Should Throw
+     }
+   }
+}
---
+++ Tests/Initialize-FilesTest.ps1
@@ -0,0 +1,19 @@
+﻿#Initialize-FilesTest.ps1
+
+if ( -not (test-path Variable:ConvertForm) )
+{ Throw 'Variable:ConvertForm do not exist.' } 
+
+Import-Module Pester
+
+if (Test-Path $Env:Temp)
+{ $Temp=$Env:Temp }
+else
+{ $Temp=[System.IO.Path]::GetTempPath() }
+
+$TestDirectory=Join-Path $Temp TestConvertForm
+Write-host "Test in $TestDirectory"
+rm $TestDirectory -rec -force -ea SilentlyContinue >$null
+md $TestDirectory -ea SilentlyContinue >$null
+
+cd $ConvertForm.Tests
+Invoke-Pester 

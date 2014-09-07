680b378a4568a6b8dd3f319cf58919927a3b8836
LaurentDardenne <LaDardenne@Orange.fr>
2014-09-07 18:05:56 +0200

    Ajout d'un projet dont le nom nécessite L'usage de LiteralPath

---
+++ TestsWinform/Test21LiteralPath[AG]Naming/Frm[AG].Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Test21LiteralPath_AG_Naming
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbProjectName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbProjectName
+            // 
+            this.lbProjectName.AutoSize = true;
+            this.lbProjectName.Location = new System.Drawing.Point(23, 35);
+            this.lbProjectName.Name = "lbProjectName";
+            this.lbProjectName.Size = new System.Drawing.Size(178, 13);
+            this.lbProjectName.TabIndex = 0;
+            this.lbProjectName.Text = "Nom de projet utilisant un Literalpath";
+            this.lbProjectName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lbProjectName);
+            this.Name = "Form1";
+            this.Text = "Frm[AG]";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbProjectName;
+    }
+}
+
---
+++ TestsWinform/Test21LiteralPath[AG]Naming/Frm[AG].cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test21LiteralPath_AG_Naming
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

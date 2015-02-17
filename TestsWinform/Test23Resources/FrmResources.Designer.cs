f0a12fc5c51fd894a0c08d8216e5bf06cf249cdd
LaurentDardenne <LaDardenne@Orange.fr>
2015-02-17 17:55:18 +0100

    Ajout d'un projet dédié aux fichiers de ressources.

---
+++ TestsWinform/Test23Resources/FrmResources.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Test23Resources
+{
+    partial class FrmResources
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmResources));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imgLstSecond = new System.Windows.Forms.ImageList(this.components);
+            this.btnClose = new System.Windows.Forms.Button();
+            this.imgLstPremier = new System.Windows.Forms.ImageList(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 317);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(515, 91);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.ImageKey = "Error.ico";
+            this.button1.ImageList = this.imgLstSecond;
+            this.button1.Location = new System.Drawing.Point(108, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // imgLstSecond
+            // 
+            this.imgLstSecond.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgLstSecond.ImageStream")));
+            this.imgLstSecond.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgLstSecond.Images.SetKeyName(0, "Warning.ico");
+            this.imgLstSecond.Images.SetKeyName(1, "Error.ico");
+            // 
+            // btnClose
+            // 
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnClose.ImageIndex = 0;
+            this.btnClose.ImageList = this.imgLstPremier;
+            this.btnClose.Location = new System.Drawing.Point(297, 23);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 56);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // imgLstPremier
+            // 
+            this.imgLstPremier.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgLstPremier.ImageStream")));
+            this.imgLstPremier.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgLstPremier.Images.SetKeyName(0, "Warning.ico");
+            this.imgLstPremier.Images.SetKeyName(1, "Error.ico");
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackgroundImage = global::Test23Resources.Properties.Resources.jpgCompression;
+            this.splitContainer1.Size = new System.Drawing.Size(515, 317);
+            this.splitContainer1.SplitterDistance = 259;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Test23Resources.Properties.Resources.jpgCompression;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmResources
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 408);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmResources";
+            this.Text = "Demo gestion des ressources";
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ImageList imgLstPremier;
+        private System.Windows.Forms.ImageList imgLstSecond;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+

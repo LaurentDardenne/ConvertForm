63c08e79a5e9785c1bc3f612147dbd8f2d539345
LaurentDardenne <LaDardenne@Orange.fr>
2017-04-10 18:45:45 +0200

    Refactoring, add build task before creating the package

--- Nuspec4ConvertForm.ps1
+++ Nuspec4ConvertForm.ps1
@@ -1,7 +1,16 @@
 ﻿if(! (Test-Path variable:ConvertFormVcs))
 { throw "The project configuration is required, see the 'ConvertForm_ProjectProfile.ps1' script." }
 
-$ModuleVersion=(Import-ManifestData "$ConvertFormVcs\ConvertForm.psd1").ModuleVersion
+Push-Location "$ConvertFormVcs\Tools"
+try {
+ .\Build.ps1
+ Import-Module PSNuspec
+}
+Finally {
+ Pop-Location        
+}
+
+$ModuleVersion=(Import-ManifestData "$ConvertFormDelivery\ConvertForm.psd1").ModuleVersion
 
 $Result=nuspec 'ConvertForm' $ModuleVersion {
    properties @{
@@ -17,24 +26,21 @@ Converting a Winform file (xxx.Designer.cs) to a PowerShell script .ps1.
         licenseUrl='https://creativecommons.org/licenses/by-nc-sa/4.0/'
         projectUrl='https://github.com/LaurentDardenne/ConvertForm'
         #iconUrl='https://github.com/LaurentDardenne/Template/blob/master/icon/ConvertForm.png'
-        releaseNotes="$(Get-Content "$ConvertFormVcs\CHANGELOG.md" -raw)"
+        releaseNotes="$(Get-Content "$ConvertFormDelivery\CHANGELOG.md" -raw)"
         tags='Form Winform Convertion'
    }
 
-   dependencies {
-        dependency Log4Posh 2.0.1
-        dependency UncommonSense.PowerShell.TypeData 1.0.2
-   }
-
    files {
-        file -src "$ConvertFormVcs\ConvertForm.psd1"
-        file -src "$ConvertFormVcs\ConvertForm.psm1"
-        file -src "$ConvertFormVcs\Transform.psm1"
-        file -src "$ConvertFormVcs\Resgen.Exe"
-        file -src "$ConvertFormVcs\Demo\" -target 'Demo\'
-        file -src "$ConvertFormVcs\Tools\Add-Header.ps1" -target 'Tools\Add-Header.ps1'
-        file -src "$ConvertFormVcs\en-US\" -target 'en-US\'
-        file -src "$ConvertFormVcs\fr-FR\" -target 'fr-FR\'
+        file -src "$ConvertFormDelivery\ConvertForm.psd1"
+        file -src "$ConvertFormDelivery\ConvertForm.psm1"
+        file -src "$ConvertFormDelivery\Transform.psm1"
+        file -src "$ConvertFormDelivery\Resgen.Exe"
+        file -src "$ConvertFormDelivery\Demo\" -target 'Demo\'
+        file -src "$ConvertFormDelivery\Tools\Add-Header.ps1" -target 'Tools\Add-Header.ps1'
+        file -src "$ConvertFormDelivery\fr-FR\" -target 'fr-FR\'
+        file -src "$ConvertFormDelivery\ConvertFormLocalizedData.psd1"
+        file -src "$ConvertFormDelivery\TransformLocalizedData.psd1"
+        file -src "$ConvertFormDelivery\CHANGELOG.md"
    }
 }
 

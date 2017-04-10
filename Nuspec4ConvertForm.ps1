9e8f9f383214dc18bf0ab55be374ff9fad6b64f6
LaurentDardenne <LaDardenne@Orange.fr>
2017-04-10 13:16:57 +0200

    Add MyGet feed

---
+++ Nuspec4ConvertForm.ps1
@@ -0,0 +1,45 @@
+﻿if(! (Test-Path variable:ConvertFormVcs))
+{ throw "The project configuration is required, see the 'ConvertForm_ProjectProfile.ps1' script." }
+
+$ModuleVersion=(Import-ManifestData "$ConvertFormVcs\ConvertForm.psd1").ModuleVersion
+
+$Result=nuspec 'ConvertForm' $ModuleVersion {
+   properties @{
+        Authors='Dardenne Laurent'
+        Owners='Dardenne Laurent'
+        Description=@'
+Creation of ps1xml file dedicated to the extension methods contained in an assembly
+'@
+        title='ConvertForm module'
+        summary='Creation of ps1xml file dedicated to the extension methods contained in an assembly.'
+        copyright='Copyleft'
+        language='fr-FR'
+        licenseUrl='https://creativecommons.org/licenses/by-nc-sa/4.0/'
+        projectUrl='https://github.com/LaurentDardenne/ConvertForm'
+        #iconUrl='https://github.com/LaurentDardenne/Template/blob/master/icon/ConvertForm.png'
+        releaseNotes="$(Get-Content "$ConvertFormVcs\CHANGELOG.md" -raw)"
+        tags='Form Winform Convertion'
+   }
+
+   dependencies {
+        dependency Log4Posh 2.0.1
+        dependency UncommonSense.PowerShell.TypeData 1.0.2
+   }
+
+   files {
+
+
+        file -src "$ConvertFormVcs\ConvertForm.psd1"
+        file -src "$ConvertFormVcs\ConvertForm.psm1"
+        file -src "$ConvertFormVcs\Transform.psm1"
+        file -src "$ConvertFormVcs\Resgen.Exe"
+        file -src "$ConvertFormVcs\Demo\" -target 'Demo\'
+        file -src "$ConvertFormVcs\Tools\Add-Header.ps1" -target 'Tools\Add-Header.ps1'
+        file -src "$ConvertFormVcs\en-US\" -target 'en-US\'
+        file -src "$ConvertFormVcs\fr-FR\" -target 'fr-FR\'
+   }
+}
+
+$Result|
+  Push-nupkg -Path $ConvertFormDelivery -Source 'https://www.myget.org/F/ottomatt/api/v2/package'
+
--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# ConvertForm
+﻿# ConvertForm
 
 ![Logo](https://github.com/LaurentDardenne/ConvertForm/blob/master/Convert-Form-Icon.jpg)
 
 Powershell module for converting a Winform file (xxx.Designer.cs) to a PowerShell script .ps1
+
+
+To install this module :
+```Powershell
+$PSGalleryPublishUri = 'https://www.myget.org/F/ottomatt/api/v2/package'
+$PSGallerySourceUri = 'https://www.myget.org/F/ottomatt/api/v2'
+
+Register-PSRepository -Name OttoMatt -SourceLocation $PSGallerySourceUri -PublishLocation $PSGalleryPublishUri #-InstallationPolicy Trusted
+Install-Module ConvertForm -Repository OttoMatt
+```
 
 
